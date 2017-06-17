e6cf3ae9404a1b673f991454dacb4f681a8b424f
Zireael07 <zi@mail.com>
2017-06-17 20:38:34 +0200

    AI now moves randomly; walls around map

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class com_Creature:
 
 class AI_test:
     def take_turn(self):
-        self.owner.move(-1,0)
+        self.owner.move(libtcod.random_get_int(0, -1, 1), libtcod.random_get_int(0, -1, 1))
 
 # MAP
 def map_create():
@@ -72,6 +72,16 @@ def map_create():
 
     new_map[10][10].block_path = True
     new_map[12][12].block_path = True
+
+    # walls around the map
+    for x in range(constants.MAP_WIDTH):
+        new_map[x][0].block_path = True
+        new_map[x][constants.MAP_WIDTH-1].block_path = True
+
+    for y in range(constants.MAP_HEIGHT):
+        new_map[0][y].block_path = True
+        new_map[constants.MAP_HEIGHT-1][y].block_path = True
+
 
     return new_map
 
@@ -153,12 +163,13 @@ def game_initialize():
 
     pygame.init()
 
-    SURFACE_MAIN = pygame.display.set_mode( [constants.GAME_WIDTH, constants.GAME_HEIGHT] )
+    SURFACE_MAIN = pygame.display.set_mode( [constants.MAP_WIDTH*constants.TILE_WIDTH,
+                                             constants.MAP_HEIGHT*constants.TILE_HEIGHT] )
 
     GAME_MAP = map_create()
 
     creature_com1 = com_Creature("Player")
-    PLAYER = obj_Actor(0, 0, "Player", constants.S_PLAYER, creature=creature_com1)
+    PLAYER = obj_Actor(1, 1, "Player", constants.S_PLAYER, creature=creature_com1)
 
     creature_com2 = com_Creature("kobold")
     ai_com = AI_test()
