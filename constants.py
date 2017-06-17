021cc9da8a69a3c97cc994ee0448985eda5bcc74
Zireael07 <zi@mail.com>
2017-06-17 16:58:21 +0200

    draw a basic 20x20 map

--- constants.py
+++ constants.py
@@ -1,3 +1,25 @@
+import pygame
 
+#Game sizes
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
+
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
+
+TILE_WIDTH = 42
+TILE_HEIGHT = 32
+
+#Color definitions
+COLOR_BLACK = (0,0,0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREY = (100, 100, 100)
+
+#Game colors
+COLOR_DEFAULT_BG = COLOR_GREY
+
+#Sprites
+
+S_PLAYER = pygame.image.load("gfx/human_m.png")
+S_WALL = pygame.image.load("gfx/wall_stone.png")
+S_FLOOR = pygame.image.load("gfx/floor_sand.png")
--- main.py
+++ main.py
@@ -3,6 +3,39 @@ import pygame
 
 import constants
 
+class struc_Tile:
+    def __init__(self, block_path):
+        self.block_path = block_path
+
+def map_create():
+    new_map = [[ struc_Tile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
+
+    new_map[10][10].block_path = True
+    new_map[12][12].block_path = True
+
+    return new_map
+
+def draw_game():
+    # clear
+    SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+    # draw map
+    draw_map(GAME_MAP)
+    # draw character
+    SURFACE_MAIN.blit(constants.S_PLAYER, (200, 200))
+
+    # update the display
+    pygame.display.flip()
+
+def draw_map(map):
+    for x in range(0, constants.MAP_WIDTH):
+        for y in range(0, constants.MAP_HEIGHT):
+            if map[x][y].block_path == True:
+                #draw wall
+                SURFACE_MAIN.blit(constants.S_WALL, (x*constants.TILE_WIDTH, y*constants.TILE_HEIGHT))
+            else:
+                #draw floor
+                SURFACE_MAIN.blit(constants.S_FLOOR, (x*constants.TILE_WIDTH, y*constants.TILE_HEIGHT))
+
 def game_main_loop():
     game_quit = False
 
@@ -15,17 +48,23 @@ def game_main_loop():
             if event.type == pygame.QUIT:
                 game_quit = True
 
-        # TODO draw
+        # draw
+        draw_game()
 
     #quit the game
     pygame.quit()
     exit()
 
+
+# Execute game
 def game_initialize():
+    global SURFACE_MAIN, GAME_MAP
 
     pygame.init()
 
     SURFACE_MAIN = pygame.display.set_mode( [constants.GAME_WIDTH, constants.GAME_HEIGHT] )
+
+    GAME_MAP = map_create()
 
 if __name__ == "__main__":
     game_initialize()
